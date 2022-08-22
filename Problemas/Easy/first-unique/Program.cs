596481e6a4cacc1807b9fb3cd9cb5f1e1908a297
Roberto Carlos <betocarlos00@hotmail.com>
2022-08-22 18:03:15 -0300

    387. First Unique Character in a String

---
+++ Problemas/Easy/first-unique/Program.cs
@@ -0,0 +1,29 @@
+﻿// Dada uma string s, encontre o primeiro caractere não repetido e retorne seu índice. Se não existir, retorne -1.
+
+class Program{
+    static void Main(string[] args){
+        var result = new Solution();
+        Console.WriteLine(result.FirstUniqChar("loveleetcode"));
+    }
+}
+
+public class Solution {
+    public int FirstUniqChar(string s) {
+        var dic = new List<char>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!dic.Contains(s[i]))
+            {
+                if (!s.Skip(i+1).Contains(s[i]))
+                {
+                    return i;
+                }
+                else
+                {
+                    dic.Add(s[i]);    
+                }
+            }
+        }
+        return -1;
+    }
+}
