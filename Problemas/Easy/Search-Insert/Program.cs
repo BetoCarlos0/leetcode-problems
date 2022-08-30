bdb6a006ebd63dc7ebfa6cd3bf6ef0ba87fdb32b
Roberto Carlos <betocarlos00@hotmail.com>
2022-08-30 17:51:08 -0300

    35. Search Insert Position commit init

---
+++ Problemas/Easy/Search-Insert/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Dada uma matriz ordenada de inteiros distintos e um valor de destino, retorne o índice se o destino for encontrado. Caso contrário, retorne o índice onde estaria se fosse inserido em ordem.
+
+Você deve escrever um algoritmo com  O(log n)complexidade de tempo de execução.
+*/
+
+class Program{
+    static void Main(string[] args){
+        var solution = new Solution();
+        int []nums = new int[] {1,3,5,6};
+        int target = 5;
+
+        Console.WriteLine(solution.SearchInsert(nums, target));
+    }
+}
+
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        
+
+        return 0;
+    }
+}
