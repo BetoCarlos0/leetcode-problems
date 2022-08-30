30894b9b52dc7d6489641c3af78ca99e0d290a7f
Roberto Carlos <betocarlos00@hotmail.com>
2022-08-30 11:32:41 -0300

    28. Implement strStr()

---
+++ Problemas/Easy/Implement-strStr/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Implemente strStr() .
+
+Dadas duas strings needleand haystack, retorne o índice da primeira ocorrência de needlein haystack, ou -1se needlenão fizer parte de haystack.
+
+Esclarecimento:
+
+O que devemos retornar quando needleé uma string vazia? Esta é uma ótima pergunta para fazer durante uma entrevista.
+
+Para o propósito deste problema, retornaremos 0 quando needlefor uma string vazia. Isso é consistente com strstr() do C e indexOf() do Java .
+*/
+
+class Program{
+    static void Main(string[] args){
+        var solution = new Solution();
+        string haystack = "hello", needle = "lle";
+
+        Console.WriteLine(solution.StrStr(haystack, needle));
+    }
+}
+
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+
+        return haystack.IndexOf(needle);
+    }
+}
