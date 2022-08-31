1583331382b8136a7162e8eda4f1bd38596272b8
Roberto Carlos <betocarlos00@hotmail.com>
2022-08-31 09:03:10 -0300

    35. Search Insert Position, final

--- Problemas/Easy/Search-Insert/Program.cs
+++ Problemas/Easy/Search-Insert/Program.cs
@@ -1,4 +1,5 @@
 ﻿/*
+35. Search Insert Position
 Dada uma matriz ordenada de inteiros distintos e um valor de destino, retorne o índice se o destino for encontrado. Caso contrário, retorne o índice onde estaria se fosse inserido em ordem.
 
 Você deve escrever um algoritmo com  O(log n)complexidade de tempo de execução.
@@ -7,8 +8,8 @@ Você deve escrever um algoritmo com  O(log n)complexidade de tempo de execuçã
 class Program{
     static void Main(string[] args){
         var solution = new Solution();
-        int []nums = new int[] {1,3,5,6};
-        int target = 5;
+        int []nums = new int[] {2,4,6,8};
+        int target = 3;
 
         Console.WriteLine(solution.SearchInsert(nums, target));
     }
@@ -17,7 +18,16 @@ class Program{
 public class Solution {
     public int SearchInsert(int[] nums, int target) {
         
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target) return i;
 
+            if (nums[i] < target && i == nums.Length-1)
+                return i+1;
+
+            if (nums[i] < target && nums[i+1] > target)
+                return i+1;
+        }
         return 0;
     }
 }
