1abbb3f1dc0c359d1b2462403f5d3494c269f43d
Roberto Carlos <betocarlos00@hotmail.com>
2022-08-31 10:31:20 -0300

    3. Longest Substring, final

---
+++ Problemas/Medium/Longest-Substring/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+3. Longest Substring Without Repeating Characters
+Dada uma string s, encontre o comprimento da substring mais longa sem repetir caracteres.
+*/
+
+class Program{
+    static void Main(string[] args){
+        var solution = new Solution();
+        string s = "dvdf";
+
+        Console.WriteLine(solution.LengthOfLongestSubstring(s));
+    }
+}
+
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        
+        char []temp = new char[s.Length];
+        int j = 1, max = 1;
+
+        if (s.Length == 0) return 0;
+        temp[0] = s[0];
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (!temp.Contains(s[i])){
+                temp[j] = s[i];
+
+            }else{
+
+                Array.Clear(temp);
+                i -= (j-1); 
+                j = 0;
+                temp[j] = s[i];
+            }
+            ++j;
+            if (j > max) ++max;
+        }
+        return max;
+    }
+}
